c2f4f1aa850e317ea91cbdf67b0581894a956c32
Robo-hub-AI <156053528+Robo-hub-AI@users.noreply.github.com>
2025-02-25 09:36:17 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let Result = 0
+let Num_1 = randint(1, 10)
+let Num_2 = randint(1, 10)
+if (Num_1 == Num_2) {
+    Result = Num_1 / Num_2
+} else {
+    Result = Num_1 * Num_2
+}
+basic.showNumber(Result)
